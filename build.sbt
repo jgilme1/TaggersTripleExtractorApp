84e9c20a17693e10619247aa14a8d526ac63206b
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-10-01 17:46:00 -0700

    first commit

---
+++ build.sbt
@@ -0,0 +1,39 @@
+import AssemblyKeys._
+
+assemblySettings
+
+organization := "edu.washington.cs.knowitall.taggers"
+
+name := "tripleExtractorApp"
+
+description := "Use taggers project to extract patterns"
+
+version := "0.2"
+
+mainClass in assembly := Some("edu.knowitall.taggers.app.TripleExtractorApp")
+
+jarName in assembly := "tripleExtractorApp.jar"
+
+crossScalaVersions := Seq("2.10.1", "2.9.3")
+
+scalaVersion <<= crossScalaVersions { (vs: Seq[String]) => vs.head }
+
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+
+libraryDependencies ++= Seq(
+    "com.google.guava" % "guava" % "13.0.1",
+    "edu.washington.cs.knowitall.nlptools" %% "nlptools-core" % "2.4.2",
+    "edu.washington.cs.knowitall.nlptools" %% "nlptools-chunk-opennlp" % "2.4.2",
+    "edu.washington.cs.knowitall.nlptools" %% "nlptools-stem-morpha" % "2.4.2",
+    "edu.washington.cs.knowitall.nlptools" %% "nlptools-typer-stanford" % "2.4.2",
+    "edu.washington.cs.knowitall" %% "openregex-scala" % "1.0.4",
+    "org.apache.commons" % "commons-lang3" % "3.1",
+    "org.jdom" % "jdom2" % "2.0.5",
+    "junit" % "junit" % "4.11" % "test",
+    "org.apache.commons" % "commons-io" % "1.3.2",
+    "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
+    "org.specs2" % "specs2" % "1.12.3" % "test" cross CrossVersion.binaryMapped {
+      case "2.9.3" => "2.9.2"
+      case "2.10.1" => "2.10"
+      case x => x
+    })                      
---
+++ src/main/java/edu/washington/cs/knowitall/taggers/TripleExtractorApp.java
@@ -0,0 +1,187 @@
+package edu.washington.cs.knowitall.taggers;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.FileUtils;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.Document;
+
+import com.google.common.io.Files;
+
+import scala.util.matching.Regex;
+import scala.xml.XML;
+
+import edu.knowitall.collection.immutable.Interval;
+import edu.knowitall.taggers.TaggerCollection;
+import edu.knowitall.taggers.pattern.TypedToken;
+import edu.knowitall.taggers.tag.Tagger;
+import edu.knowitall.tool.chunk.ChunkedToken;
+import edu.knowitall.tool.chunk.OpenNlpChunker;
+import edu.knowitall.tool.stem.Lemmatized;
+import edu.knowitall.tool.stem.MorphaStemmer;
+import edu.knowitall.tool.typer.Type;
+
+public class TripleExtractorApp {
+	
+	
+	
+	public static void main(String[] args) throws SecurityException, IllegalArgumentException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, JDOMException, IOException{
+		if(args.length != 3){
+			System.err.println("App takes exactly 3 arguments:\n" +
+								"1. path to patterntagger specification file\n" +
+								"2. path to input text file with one sentence per line\n" +
+								"3. path to output file");
+			return;
+		}
+			String taggerPath = args[0];
+			String inputPath = args[1];
+			String outputPath = args[2];
+			
+			String patternString = FileUtils.readFileToString(new File(taggerPath));
+			//patternString = refactorPatterns(patternString);
+			
+			
+			TaggerCollection t = TaggerCollection.fromString(patternString);
+			List<Tagger> taggers = scala.collection.JavaConversions.asJavaList(t.taggers());
+			String[] taggerDescriptors = new String[taggers.size()];
+			for(int i = 0; i < taggerDescriptors.length; i++){
+				taggerDescriptors[i] = taggers.get(i).name();
+			}
+									
+			String inputFileString = FileUtils.readFileToString(new File(inputPath));
+			
+			
+			
+			PrintWriter pw = new PrintWriter(new File(outputPath));
+
+			
+			writeHeader(pw,taggerDescriptors);
+	        OpenNlpChunker chunker = new OpenNlpChunker();
+	        MorphaStemmer morpha = new MorphaStemmer();
+		
+			for(String line : inputFileString.split("\n")){
+				
+	            List<ChunkedToken> chunkedSentence = scala.collection.JavaConverters.seqAsJavaListConverter(chunker.chunk(line)).asJava();
+	            List<Lemmatized<ChunkedToken>> tokens = new ArrayList<Lemmatized<ChunkedToken>>(chunkedSentence.size());
+	            for (ChunkedToken token : chunkedSentence) {
+	                Lemmatized<ChunkedToken> lemma = morpha.lemmatizeToken(token);
+	                tokens.add(lemma);
+	            }
+	            pw.write(line.trim()+"\t");
+	            for(Lemmatized<ChunkedToken> tok: tokens){
+	            	pw.write(tok.token().postag()+" ");
+	            }	            
+	          //  List<Type> types = t.tag(scala.  tokens);
+	            List<Type> types = scala.collection.JavaConversions.asJavaList(t.tag(scala.collection.JavaConversions.asScalaBuffer(tokens).seq()));
+	            System.out.println(line);
+	            
+
+	            
+	            
+	            for(String level: taggerDescriptors){
+		            pw.write("\t");
+		            
+            		List<Type> relevantTypes = new ArrayList<Type>();
+
+	            	for(Type type: types){
+						if(type.name().startsWith(level)){
+	            			relevantTypes.add(type);
+	            		}
+	            	}
+	            		
+		            java.util.Collections.sort(relevantTypes, new Comparator<Type> (){
+	            	public int compare(Type t1, Type t2){
+	            		return t1.tokenInterval().compare(t2.tokenInterval());
+	            	}
+		            });
+		            
+		            for(Type type: relevantTypes){
+		              if(type.name().equals(level)) pw.write(type + " ");
+		            }
+	            		//if we are at the highest level write group matches as well
+            		if(taggerDescriptors[taggerDescriptors.length-1] == level){
+            			for(Type type: relevantTypes){
+		            		if(!type.name().equals(level)){
+		            			pw.write("\t"+type.name().substring(level.length()+1)+":"+type.text());
+		            		}
+            			}
+            			pw.write("\t");
+            		}
+	            }
+	            pw.write("\n");
+			}				
+			
+			
+			pw.close();
+	}
+	
+//	private static String refactorPatterns(String patternString){
+//		Pattern typePattern = Pattern.compile("<type=([^>]+)>\\+");
+//		
+//		
+//		Matcher typePatternMatcher = typePattern.matcher(patternString);
+//		int start =0;
+//		while(start < patternString.length() && typePatternMatcher.find(start)){
+//			String group1 = typePatternMatcher.group(1);
+//			String replacementString = "(<typeStart="+group1+" & typeEnd="+group1+"> | (<typeStart="+group1+"> <type="+group1+">* <typeEnd="+group1+">))";
+//			patternString = patternString.substring(0, typePatternMatcher.start()) + replacementString + patternString.substring(typePatternMatcher.end());
+//			start = typePatternMatcher.start() + replacementString.length();
+//			typePatternMatcher = typePattern.matcher(patternString);
+//		}
+//		
+//		return patternString;
+//	}
+
+	private static void writeHeader(PrintWriter pw, String[] taggerList){		 
+		pw.write("sentence\tpos");
+		
+		for(String subDirName: taggerList){
+			pw.write("\t"+subDirName+"-tags");
+		}
+		
+		pw.write("\tPattern components\n");
+	}
+	
+	
+	private static String findLevel(Type type, File taggerDirectory) throws IOException{
+		
+		List<String> typeDescriptors = new ArrayList<String>();
+		typeDescriptors.add(type.name());
+		if(type.source() != null){
+			typeDescriptors.add(type.source());
+		}
+		
+		for(File subDir : taggerDirectory.listFiles()){
+			if(subDir.isDirectory()){
+				String level = subDir.getName();
+				for(File x: subDir.listFiles()){
+					String xString = FileUtils.readFileToString(x);
+					for(String descriptionString : typeDescriptors){
+						if(xString.indexOf("descriptor=\""+descriptionString+"\">") != -1){
+							return level;
+						}
+						
+					}
+				}
+			}
+		}
+		
+		return "-1";
+	}
+}
+			
